a389a41bdb68bd19c8d474ce287cca20e0e05543
YalcinKurt89 <yalcin.coding@gmail.com>
2023-12-06 10:35:11 +0100

    update game.js

--- game.js
+++ game.js
@@ -1,17 +1,30 @@
 
-var buttonColours = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
-var userClickedPattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 
 
 $(".btn").click(function() {
 
-  var userChosenColour = $(this).attr("id");
+  let userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
-
   animatePress(userChosenColour);
 });
 
@@ -19,39 +32,30 @@ $(".btn").click(function() {
 
 function nextSequence() {
 
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
+  level++;
+
+  $("#level-title").text("Level " + level);
+
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
-};
+}
 
 
 
 function playSound(name) {
-
-  var audio = new Audio("sounds/" + name + ".mp3");
+  let audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
-};
+}
 
 
 
-function animatePress(currentColour) {
-
-  $('#' + currentColour).addClass("pressed");
-
-  setTimeout(function() {
-    $('#' + currentColour).removeClass("pressed");
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
   }, 100);
-};
-
-
-$(document).on("keydown", function() {
-  nextSequence();
-});
-
-var level = 0;
-
-$("h1").text("Level" + level)
+}
